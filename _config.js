84f0f92f67c800fa450dd9e9ee2ac2201c8ae5a6
jeonghye-choi <ppllhm.0@gmail.com>
2021-01-28 03:21:12 +0900

    jh blog main theme

---
+++ _config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  /** Site MetaData (Required all)*/
+  title: `정혜 블로그`,                           // (* Required)
+  description: `공부하고 생각한 것을 적는 공간입니다.`,          // (* Required)
+  author: `Jeonghye Choi`,                         // (* Required)
+  language: 'ko-KR',                        // (* Required) html lang, ex. 'en' | 'en-US' | 'ko' | 'ko-KR' | ...
+  siteUrl: 'https://jeonghye-choi.github.io',                      // (* Required)
+    // ex.'https://junhobaik.github.io'
+    // ex.'https://junhobaik.github.io/' << X, Do not enter "/" at the end.
+
+  /** Header */
+  profileImageFileName: 'profileImg.jpg', // include filename extension ex.'profile.jpg'
+    // The Profile image file is located at path "./images/"
+    // If the file does not exist, it is replaced by a random image.
+
+  /** Home > Bio information*/
+  comment: 'Enter your information in the "./config.js" file. ',
+  name: 'Jeonghye',
+  company: '',
+  location: 'Korea',
+  email: 'ppllhm.0@gmail.com',
+  website: 'https://github.com/jeognhye-choi/jeognhye-choi.github.io',           // ex.'https://junhobaik.github.io'
+  linkedin: '',                                                          // ex.'https://www.linkedin.com/in/junho-baik-16073a19ab'
+  facebook: '',                                                          // ex.'https://www.facebook.com/zuck' or 'https://www.facebook.com/profile.php?id=000000000000000'
+  instagram: 'https://instagram.com/jihyenanum',                                                         // ex.'https://www.instagram.com/junhobaik'
+  github: 'https://github.com/jeognhye-choi',                                                            // ex.'https://github.com/junhobaik'
+
+  /** Post */
+  enablePostOfContents: true,     // TableOfContents activation (Type of Value: Boolean. Not String)
+  disqusShortname: '',            // comments (Disqus sort-name)
+  enableSocialShare: true,        // Social share icon activation (Type of Value: Boolean. Not String)
+
+  /** Optional */
+  googleAnalytics: '',     // Google Analytics TrackingID. ex.'UA-123456789-0'
+  googleSearchConsole: '', // content value in HTML tag of google search console ownership verification. ex.'w-K42k14_I4ApiQKuVPbCRVV-GxlrqWxYoqO94KMbKo'
+  googleAdsenseSlot: '',   // Google Adsense Slot. ex.'5214956675'
+  googleAdsenseClient: '', // Google Adsense Client. ex.'ca-pub-5001380215831339'
+    // Please correct the adsense client number(ex.5001380215831339) in the './static/ads.txt' file.
+};
